a123a61a38802440fba82199b18cd4a32de68c62
JOSEBA URIZARBARRENA HONTORIA TSAPP2TA2021 <joseba_urizarbarrena_apps1ta1920@cev.com>
2020-12-01 17:10:03 +0100

    Archivo web con tres rutas

--- laravel-git/routes/web.php
+++ laravel-git/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\ViewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','ViewController@getForm');
+
+Route::post('/uploadFile','ViewController@uploadFile');
+
